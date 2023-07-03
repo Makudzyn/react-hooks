e41002ae2c671bd0a827d06889d3201c1f6c80e1
Maksim Kunchenko <72383565+Makudzyn@users.noreply.github.com>
2023-07-03 13:26:16 +0300

    Created 3 custom hooks and components to try them

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Hover from "./components/Hover.jsx";
+import List from "./components/List.jsx";
+
+function App() {
+
+  return (
+    <>
+      <List/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hover.jsx
@@ -0,0 +1,14 @@
+import React, {useRef} from 'react';
+import useHover from "../hooks/useHover.js";
+
+const Hover = () => {
+  const ref = useRef();
+  const isHovering = useHover(ref);
+
+  return (
+    <div ref={ref} style={{width: "300px", height: "300px", background: "#fab800", border: isHovering ? "1px solid black" : "none"}}>
+    </div>
+  );
+};
+
+export default Hover;
