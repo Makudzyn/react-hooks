a8c636f143e1ca8c74e21f870284edcc8d631baf
Maksim Kunchenko <72383565+Makudzyn@users.noreply.github.com>
2023-07-05 16:30:21 +0300

    created two more hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
-import Hover from "./components/Hover.jsx";
-import List from "./components/List.jsx";
+import axios from "axios";
+import useRequest from "./hooks/useRequest.js";
 
 function App() {
+  const [todos, loading, error] = useRequest(fetchTodos);
+  function fetchTodos() {
+    return axios.get(`https://jsonplaceholder.typicode.com/todos`);
+  }
+
+  if (loading) return <h1>Loading...</h1>
+  if (error) return <h1>An error has occurred.</h1>
 
   return (
     <>
-      <List/>
+        {todos && todos.map(todo =>
+          <div key={todo.id} style={{padding: 30, border: "2px solid black"}}>
+            {todo.id}.
+            {todo.title}
+          </div>
+        )}
     </>
   )
 }
